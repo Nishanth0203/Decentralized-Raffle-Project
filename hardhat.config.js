394f6c3aff8378676603f2766a7b2aab1a8d62b6
Nishanth0203 <dugyalanishanthrao@gmail.com>
2022-12-29 17:11:32 +0530

    RAFFLE-PROJECT

---
+++ hardhat.config.js
@@ -0,0 +1,41 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli.g.alchemy.com/v2/BIUhUGSSQYE6n4Tlm3e5n7U6rJG5RuA_"
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmtions: 1,
+    },
+  },
+
+  gasReporter: {
+    enabled: false,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+  },
+
+  solidity: "0.8.17",
+  namedAccounts: {
+    deployer: {
+        default: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
+}
---
+++ helper-hardhat-config.js
@@ -0,0 +1,26 @@
+const networkConfig = {
+    5: {
+        name: "goerli",
+        vrfCoordinatorV2: "0xc587d9053cd1118f25F645F9E08BB98c9712A4EE",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0x114f3da0a805b6a67d6e9cd2ec746f7028f1b7376365af575cfea3550dd1aa04",
+        SUBSCRIPTIONid: "0",
+        callbackGasLimit: "500000",
+        interval: "30"
+    },
+    31337: {
+        name: "localhost",
+        subscriptionId: "588",
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
+        keepersUpdateInterval: "30",
+        raffleEntranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
+        callbackGasLimit: "500000", // 500,000 gas
+    },
+}
+
+const developmentChains = ["hardhat", "localhost"]
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+}
