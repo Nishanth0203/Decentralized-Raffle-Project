5741960c9b38da8eecacb1d29999e5c0ee4a3b4b
Nishanth0203 <dugyalanishanthrao@gmail.com>
2023-01-02 19:28:28 +0530

    updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,6 +16,12 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
+    goerli: {
+      chainId: 5,
+      blockConfirmations: 1,
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY]
+    },
     hardhat: {
       chainId: 31337,
       blockConfirmtions: 1,
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -4,7 +4,7 @@ const networkConfig = {
         vrfCoordinatorV2: "0xc587d9053cd1118f25F645F9E08BB98c9712A4EE",
         entranceFee: ethers.utils.parseEther("0.01"),
         gasLane: "0x114f3da0a805b6a67d6e9cd2ec746f7028f1b7376365af575cfea3550dd1aa04",
-        SUBSCRIPTIONid: "0",
+        SUBSCRIPTIONid: "8174",
         callbackGasLimit: "500000",
         interval: "30"
     },
